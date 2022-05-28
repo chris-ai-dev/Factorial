cb792a40d63dd1ef3d96330e98298fb6e4282cf7
Chris <chrisvolar@gmail.com>
2022-05-28 15:48:59 -0500

    recursion

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,24 @@
 exports.factorial = function(num) {
+    if(num === 0) {
+        return 1;
+    } else {
+        return exports.factorial(num-1) * num;
+    }
 };
+
+// function factorial(num) {
+//     if(num ===0 ) {
+//         return 1
+//     }
+//     //loop down from number and multiply
+//     let sum = 1;
+//     for(num; num  > 0; num-- ){
+//         //console.log(num);
+//         sum *= num;
+//     }
+//     return sum;
+// };
+
+// console.log(factorial(45))
+
+
--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,6 @@
 def factorial(num):
 	# your code here
+	if num == 0:
+		return 1
+	else:
+		return factorial(num-1) * num
